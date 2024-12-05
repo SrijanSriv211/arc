e5772ed8cca07d23924925b6e9e4e28cd6a0b983
SrijanSriv211 <QCoreNest@gmail.com>
2024-12-05 23:04:19 +0530

    Update arcpch.h

--- src/arcpch.h
+++ src/arcpch.h
@@ -2,17 +2,8 @@
 
 #include <iostream>
 #include <filesystem>
-#include <functional>
-#include <algorithm>
-#include <fstream>
-#include <cstring>
-#include <iomanip>
 #include <vector>
-#include <cctype>
-#include <format>
 #include <string>
 #include <chrono>
-#include <cmath>
-#include <map>
 
 #include <windows.h>
