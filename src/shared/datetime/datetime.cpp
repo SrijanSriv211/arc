dfdb4e8e2d6ed98685bb5a53443353fd66e86ab0
SrijanSriv211 <QCoreNest@gmail.com>
2024-12-05 23:05:04 +0530

    Create shared datetime functions

---
+++ src/shared/datetime/datetime.cpp
@@ -0,0 +1,16 @@
+#include "arcpch.h"
+#include "datetime.h"
+
+namespace datetime
+{
+    std::string datetime(const std::string& format)
+    {
+        const auto now = std::chrono::system_clock::now();
+        const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+
+        std::tm now_tm = *std::localtime(&t_c);
+        std::stringstream ss;
+        ss << std::put_time(&now_tm, format.c_str());
+        return ss.str();
+    }
+}
