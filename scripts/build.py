6e32b0a448fb2c5484782a62ff96932a3e5d3c79
SrijanSriv211 <QCoreNest@gmail.com>
2024-12-05 22:18:53 +0530

    Build arc

---
+++ scripts/build.py
@@ -0,0 +1,33 @@
+import os
+
+# https://stackoverflow.com/a/18351977/18121288
+# Though not widely known, str.endswith also accepts a tuple. You don't need to loop.
+def get_files(path: str, ext: tuple):
+    return [os.path.join(path, file) for path, _, files in os.walk(path) for file in files if file.endswith(ext)]
+
+def join(lst, prefix="", separator=" "):
+    return separator.join([prefix + i for i in lst])
+
+CONFIG = {
+    "SRC_PATH": "src",
+    "ICON_PATH": "src/ico.o",
+    "INCLUDES": ["src/", "src/shared/"],
+    "DEFINES": ["VERSION=1"],
+    "EXTRAS": "-lws2_32",
+    "STD": "c++20",
+    "OUTPATH": "bin\\arc.exe",
+    "OPTIMIZATION": "-O2",
+    "PRECOMPILES": [
+        ("src/arcpch.h.gch", "g++ src/arcpch.h"), # compile arc precompiled headers
+        ("src/ico.o", "windres src/ico.rc -O coff -o src/ico.o") # create ico.o containing the data for arc icon
+    ]
+}
+COMMON = f"{join(CONFIG["INCLUDES"], "-I")} {CONFIG["EXTRAS"]} {join(CONFIG["DEFINES"], "-D")} {CONFIG["OPTIMIZATION"]} -std={CONFIG["STD"]} -Wall"
+
+[os.system(cmd) for path, cmd in CONFIG["PRECOMPILES"] if not os.path.isfile(path)]
+
+obj_files = get_files(CONFIG["SRC_PATH"], (".cpp", ".c"))
+COMMAND = f"g++ {CONFIG["ICON_PATH"]} {join(obj_files)} {COMMON} -o {CONFIG["OUTPATH"]}"
+
+print(COMMAND)
+os.system(COMMAND)
