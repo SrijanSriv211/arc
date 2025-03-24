599c3ecaa46e5a61a2979e40ff465b1fa3f270c7
SrijanSriv211 <QCoreNest@gmail.com>
2025-03-24 16:47:21 +0530

    include and link libcurl in project

--- scripts/build.py
+++ scripts/build.py
@@ -1,4 +1,4 @@
-import sys, os
+import shutil, sys, os
 
 # https://stackoverflow.com/a/18351977/18121288
 # Though not widely known, str.endswith also accepts a tuple. You don't need to loop.
@@ -16,24 +16,30 @@ CONFIG = {
     "SRC_PATH": "src",
     "ICON_PATH": "src/ico.o",
     "INCLUDES": ["src/", "src/core/", "src/shared/", "src/vendor/"],
-    "EXTRAS": "-lws2_32",
+    "LINKERS": ["src/vendor/libcurl/lib"],
+    "EXTRAS": "-lws2_32 -llibcurl",
     "STD": "c++20",
-    "OUTPATH": "bin\\arc.exe",
+    "DLLPATH": "src\\vendor\\libcurl\\bin",
+    "OUTPATH": "bin\\",
+    "OUTNAME": "arc.exe",
     "OPTIMIZATION": "-O2",
     "PRECOMPILES": [
         ("src/arcpch.h.gch", "g++ src/arcpch.h"), # compile arc precompiled headers
         ("src/ico.o", "windres src/ico.rc -O coff -o src/ico.o") # create ico.o containing the data for arc icon
     ]
 }
-COMMON = f"{join(CONFIG["INCLUDES"], "-I")} {CONFIG["EXTRAS"]} {CONFIG["OPTIMIZATION"]} -std={CONFIG["STD"]} -Wall"
+COMMON = f"{join(CONFIG["INCLUDES"], "-I")} {join(CONFIG["LINKERS"], "-L")} {CONFIG["EXTRAS"]} {CONFIG["OPTIMIZATION"]} -std={CONFIG["STD"]} -Wall"
 
 if not os.path.isdir(os.path.dirname(CONFIG["OUTPATH"])):
-    os.mkdir(os.path.dirname(CONFIG["OUTPATH"]))
+    os.makedirs(os.path.dirname(CONFIG["OUTPATH"]))
 
 if len(sys.argv) <= 1:
     [system(cmd) for path, cmd in CONFIG["PRECOMPILES"] if not os.path.isfile(path)]
 
     obj_files = get_files(CONFIG["SRC_PATH"], (".cpp", ".c"))
-    system(f"g++ {CONFIG["ICON_PATH"]} {join(obj_files)} {COMMON} -o {CONFIG["OUTPATH"]}")
+    system(f"g++ {CONFIG["ICON_PATH"]} {join(obj_files)} {COMMON} -o {CONFIG["OUTPATH"] + CONFIG["OUTNAME"]}")
 
-os.system(f"call {CONFIG["OUTPATH"]}")
+    dll_files = get_files(CONFIG["DLLPATH"], (".dll"))
+    [shutil.copy2(file, CONFIG["OUTPATH"]) for file in dll_files]
+
+os.system(f"call {CONFIG["OUTPATH"] + CONFIG["OUTNAME"]}")
